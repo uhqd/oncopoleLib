001b0e96c4b6d7acb5b7cf7a10ef06a99a478c9a
uhqd <uhqd75@gmail.com>
2026-02-24 17:19:11 +0100

    Ajoutez des fichiers projet.

---
+++ contextMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+namespace oncopoleLib
+{
+    public class contextMaster
+    {
+        public bool aPlanIsloaded { get; set; }
+        public contextMaster(ScriptContext ctx)
+        {
+            aPlanIsloaded = true;
+            if (ctx.PlanSetup == null)
+            {
+                aPlanIsloaded = false;
+            }
+        }
+    }
+}
